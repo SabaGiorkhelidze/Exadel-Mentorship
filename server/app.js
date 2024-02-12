4408fcc224a2be9e345d07de2c294e05dceeb6d5
Saba Giorkhelidze <sabagiorkhelidze@gmail.com>
2024-02-13 00:25:52 +0400

    created mongodb and connect it to atlas vith mongoose

--- server/app.js
+++ server/app.js
@@ -1,15 +1,23 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import Mongoose from "mongoose";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.get("/", async (request, response) => {
+  response.status(200).send("hello");
+});
 
-app.get('/', async (request, response) => {
-    response.status(200).send('hello')
-})
+const MongoURL =
+  "mongodb+srv://sabagiorkhelidze:exadel@ecommerce.xzoob7x.mongodb.net/?retryWrites=true&w=majority";
 
-app.listen(8080, () => {
-    console.log('app is listening to post 8080')
-})
+Mongoose.connect(MongoURL)
+  .then(() => {
+    console.log("App connected to db");
+    app.listen(8080, () => {
+      console.log("app is listening to post 8080");
+    });
+  })
+  .catch((err) => console.log(err));
