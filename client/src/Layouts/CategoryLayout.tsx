2ff69a10181d948217e02b31970a8a88529c0dd8
Saba Giorkhelidze <sabagiorkhelidze@gmail.com>
2024-02-15 18:25:29 +0400

    declared functions for arrow scrolling

--- client/src/Layouts/CategoryLayout.tsx
+++ client/src/Layouts/CategoryLayout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import CategoryCard from "../Components/Cards/CategoryCard";
 import { CiMonitor } from "react-icons/ci";
 import { CiMicrochip } from "react-icons/ci";
@@ -63,6 +63,22 @@ const categoryCardData: CategoryCardDataProps[] = [
 ];
 
 const CategoryLayout = () => {
+  const [scrollPosition, setScrollPosition] = useState(0); 
+  const cardsRef = useRef<HTMLDivElement | null>(null);
+
+  const cardsPerPage = 3;
+
+  const handleLeftScroll = () => {
+    if (scrollPosition > 0) {
+      setScrollPosition(scrollPosition - cardsPerPage);
+    }
+  };
+
+  const handleRightScroll = () => {
+    if (scrollPosition < cardsRef.current?.scrollWidth - cardsPerPage * cardsRef.current?.clientWidth) {
+      setScrollPosition(scrollPosition + cardsPerPage);
+    }
+  };
   return (
     <div>
       <div className="flex flex-row space-x-4 overflow-x-auto scrollbar-smooth">
