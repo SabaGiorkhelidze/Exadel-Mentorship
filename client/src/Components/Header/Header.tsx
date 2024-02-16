3412652cd73bba00d065d9f16ccd30a1a9289e79
Saba Giorkhelidze <sabagiorkhelidze@gmail.com>
2024-02-16 23:00:34 +0400

    fixed minor style issue

--- client/src/Components/Header/Header.tsx
+++ client/src/Components/Header/Header.tsx
@@ -2,10 +2,21 @@ import React from "react";
 
 interface HeaderPropTypes {
   title: string;
+  subTitle: string;
 }
 
-const Header = ({ title }: HeaderPropTypes) => {
-  return <div><h1 className="text-xl font-semibold">{title}</h1></div>;
+const Header = ({ title, subTitle }: HeaderPropTypes) => {
+  return (
+    <div className="border-2 border-black">
+      <div className="flex flex-row">
+        <div className="w-4 h-8 bg-red-500 rounded-sm mr-4"></div>
+      <h1 className="text-xl font-semibold text-red-500">{subTitle}</h1>
+      </div>
+      <div>
+        <h1 className="text-xl font-semibold">{title}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- client/src/Pages/HomePage.tsx
+++ client/src/Pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import Banner from "../Components/Banner/Banner"
+import Header from "../Components/Header/Header"
 import SideNavbar from "../Components/SideNavbar/SideNavbar"
 import HomeHeaderLayout from "../Layouts/HomePageLayouts/HomeHeaderLayout"
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="mx-10 border-2 border-black ">
         <HomeHeaderLayout />
+        <Header title="Flashes" subTitle="red text"/>
     </div>
   )
 }
